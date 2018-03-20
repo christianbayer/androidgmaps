1a1921141b056b532183b5272b851a9106fc636a
Christian Bayer <christian.bayer@universo.univates.br>
2018-03-20 19:57:37 -0300

    Mapa funcionando e marcando trajeto, atualizado gradle

--- app/src/main/java/com/example/christian/fitapp/MapsActivity.java
+++ app/src/main/java/com/example/christian/fitapp/MapsActivity.java
@@ -1,8 +1,11 @@
 package com.example.christian.fitapp;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -16,10 +19,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.provider.Telephony.Mms.Part.FILENAME;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    public List<LatLng> routeArray = new ArrayList<LatLng>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,15 +70,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Criteria criteria = new Criteria();
         String provider = locationManager.getBestProvider(criteria, true);
         Location location = locationManager.getLastKnownLocation(provider);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
 
-        Log.d("MyActivity", "Aqui");
         if (location != null) {
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
 
             LatLng coordinate = new LatLng(latitude, longitude);
-            CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate, 19);
+            CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate, 6);
             mMap.animateCamera(yourLocation);
         }
+    }
+
+    public void drawLines () {
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .width(3)
+                .color(Color.BLUE)
+                .geodesic(true));
+        line.setPoints(routeArray);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            routeArray.add(new LatLng(location.getLatitude(), location.getLongitude()));
+            if (routeArray.size() > 0) {
+                drawLines();
+            }
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
